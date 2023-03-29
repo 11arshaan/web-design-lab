5024b4f9d564343202cf892acb352b91786648d2
Harshaan <harshaan.work@gmail.com>
2023-03-29 10:22:31 -0400

    added galaxy generator

--- src/routes/WebGLRoute.jsx
+++ src/routes/WebGLRoute.jsx
@@ -1,10 +1,12 @@
 import WebGLSketch from "../components/webGL/webGL-template/WebGLsketch";
+import ThreeGalaxyGenerator from "../components/webGL/three-galaxygenerator/ThreeGalaxyGenerator";
 import ComponentCard from "../ui/ComponentCard/ComponentCard";
 
 export default function WebGLRoute() {
   return (
   <div className="container">
   <ComponentCard component={WebGLSketch} title="WebGL Sketch" description={"A template for starting WebGL designs in react."} />
+  <ComponentCard component={ThreeGalaxyGenerator} title="Galaxy Generator" description={"An interactive galaxy generator made with Three.js"} />
 
   </div>);
 }
